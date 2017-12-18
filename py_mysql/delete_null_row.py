29809d6b2b15a62a5eb3709084385668f7b1bf91
鹿野 孟城 <shikano.takeki@nxj.jp>
2017-12-18 14:12:16 +0900

    first commit

---
+++ py_mysql/delete_null_row.py
@@ -0,0 +1,177 @@
+#-------------------------------------------------------------------------------
+# Name:        delete_null_row.py
+# Purpose:     テキストファイルを読み込み、空白行を削除した文字列リストを返す。
+#
+# Author:      shikano.takeki
+#
+# Created:     08/12/2017
+# Copyright:   (c) shikano.takeki 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+# -*- coding: utf-8 -*-
+import os
+import re
+import codecs
+
+class DeleteNullRow:
+    """DeleteNullRow
+
+    """
+    def __init__(self):
+        """コンストラクタ
+
+        :param encode: 文字エンコーディング.
+        """
+        self.list_str = list()
+        self.with_combined = ''
+
+    def read_text_file(self, dir_path: str, encode=None):
+        """ファイルを読み込んで空白行を削除した文字列群をリストに格納し返す.
+
+        :param dir_path: ディレクトリパス.
+        :param encode: 文字エンコーディングの指定 デフォルトはUTF-8.
+        """
+        if encode is None:
+            encode = 'utf_8'
+        # 挿入文字列を格納しておくリスト
+        ins_str = list()
+        # 連結用文字列を格納しておく.
+        joined_strs = ''
+        joined_str = tuple()
+        try:
+            with codecs.open(dir_path, mode='r', encoding=encode) as file:
+                for line in file:
+                    if line in {'\n', '\r', '\r\n' }:
+                        continue
+                    elif self.is_matched(line=line, search_obj="^-+"):
+                        continue
+                    else:
+                        joined_strs = self.join_lines(line)
+                        if not len(joined_strs) == 0:
+                            ins_str.append(joined_strs)
+                        """
+                        joined_str += (line.rstrip().split(),)
+                        if not line.rstrip()[-1] == ';':
+                            continue
+                        for list_element in joined_str:
+                            joined_strs += ' '.join(list_element) + ' '
+                        print("joined_strs = {}".format(joined_strs))
+                        ins_str.append(joined_strs.rstrip())
+                        joined_str = tuple()
+                        joined_strs = ''
+                        """
+        except FileNotFoundError as e:
+            print("\n存在しないファイルです。パス指定が正しいかどうか確認してください。\n")
+            raise e
+        except UnicodeError as e:
+            print("\nエラー原因: " + e.reason)
+            print("文字エンコーディングの指定を変更してみるといいかもしれない\n")
+            raise e
+        except (LookupError, ValueError) as e:
+            print("存在しない、または無効な値です。")
+            raise e
+        else:
+            print("==========================================\n")
+            print("Complete loading a file.\n")
+            return ins_str
+
+    def is_matched(self, line: str, search_obj: str):
+        """引数で渡されたパターンに基づいて文字列を捜索する.
+
+        Args:
+            param1 line: テキストファイル1行分にあたる文字列.
+            param2 search_obj: 捜索パターン.
+
+        Returns:
+            パターンにマッチしたならTrue そうでないならFalseを返す.
+        """
+        match_obj = re.match(search_obj, line)
+        if match_obj is not None:
+            return True
+        else:
+            return False
+    def join_lines(self, line: str):
+        """引数で渡された文字列を条件に基づいて処理をする.
+
+        セミコロンまでを１行とみなす。
+        行の終端がセミコロンの行までを１行に連結する。
+
+        AAAA
+        BBBBBBB;
+
+        上の例では、AAAABBBBBBB;を１行として出力する.
+
+        Args:
+            param1 line: テキストファイル１行分にあたる文字列.
+
+        Returns:
+            1行とみなした文字列を返す.
+        """
+        self.list_str.append(line.rstrip().split())
+        if not line.rstrip()[-1] == ';':
+            return ''
+        for list_element in self.list_str:
+            self.with_combined += ' '.join(list_element) + ' '
+        self.list_str = list()
+        ret_str = self.with_combined
+        self.with_combined = ''
+        return ret_str.rstrip()
+
+    def write_text_file(self, str_line: str, dir_path: str, mode: str, encode=None):
+        """引数で受け取った文字列をファイルに書き込む.
+
+        :param str_line: 書き込む文字列.
+        :param dir_path: 出力ファイルのパス.
+        :param mode: 書き込みモード
+                     'w' = 上書きモード
+                     'a' = 追記モード
+        :param encode: 文字エンコーディングの指定 デフォルトはUTF-8.
+        """
+        if encode is None:
+            encode = 'utf_8'
+        try:
+            with codecs.open(r'{}'.format(dir_path), mode=mode, encoding=encode) as file:
+                file.write(str(str_line))
+        except FileNotFoundError as e:
+            print("\n存在しないファイルです。パス指定が正しいかどうか確認してください。\n")
+            raise e
+        except UnicodeError as e:
+            print("\nエラー原因: " + e.reason)
+            print("文字エンコーディングの指定を変更してみるといいかもしれない\n")
+            raise e
+        except (LookupError, ValueError) as e:
+            print("存在しない、または無効な値です。")
+            raise e
+        else:
+            pass
+
+    def is_opened(self, dir_path: str, mode: str, encode=None):
+        """
+        ファイルがオープンできるかどうかの検査用メソッド.
+
+        :param dir_path: ディレクトリパス.
+        :param mode: オープンモード.
+        :param encode: 文字エンコーディング.
+        """
+        if encode is None:
+            encode = 'utf_8'
+        try:
+            with codecs.open(r'{}'.format(dir_path), mode=mode, encoding=encode) as file:
+                for line in file:
+                    pass
+        except FileNotFoundError as e:
+            print("\n存在しないファイルです。パス指定が正しいかどうか確認してください。\n")
+            return False
+        except UnicodeError as e:
+            print("エラー発生\nエラー原因: " + e.reason)
+            print("文字エンコーディングの指定を変更してみるといいかもしれない\n")
+            return False
+        except (LookupError, ValueError, OSError) as e:
+            print("存在しない、または無効な値です。")
+            return False
+        else:
+            if mode == 'w' or mode == 'a' and os.path.isfile(dir_path):
+                os.remove(dir_path)
+            return True
+
+
